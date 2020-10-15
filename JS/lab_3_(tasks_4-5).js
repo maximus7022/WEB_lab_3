996e25f0e8af03097eca46dce59574f3d85cddc9
maximus7022 <pash6790@gmail.com>
2020-10-15 20:35:13 +0300

    Solving issues with reference to form and task 5

--- JS/lab_3_(tasks_4-5).js
+++ JS/lab_3_(tasks_4-5).js
@@ -7,83 +7,85 @@
 // surname, age, course та властивостями GetOlder()- зміна віку на
 // вказану кількість років, ChangeSurname() - зміна прізвища,
 // MoveToSecondCourse() – переведення на наступний курс.
+$('document').ready(function() {
 
-var Student = new Object();
+	var Student = new Object();
 
-Student.name = "Максим";
-Student.surname = "Пашинський";
-Student.age = 18;
-Student.course = 2;
+	Student.name = "Максим";
+	Student.surname = "Пашинський";
+	Student.age = 18;
+	Student.course = 2;
 
-$('#button_age').on('click', function GetOlder() {
-	if( $('#input_age').val() == '')
-	{
-		alert("Нічого не введено");
-	}
-	else
-	{
-		Student.age = $('#input_age').val();
-		alert('Новий вік студента: ' + Student.age);
-	}
-});
+	$('#button_age').on('click', function GetOlder() {
+		if( $('#input_age').val() == '')
+		{
+			alert("Нічого не введено");
+		}
+		else
+		{
+			Student.age = $('#input_age').val();
+			alert('Новий вік студента: ' + Student.age);
+		}
+	});
 
-$('#button_name').on('click', function ChangeName() {
-	if( $('#input_name').val() == '')
-	{
-		alert("Нічого не введено");
-	}
-	else
-	{
-		Student.name = $('#input_name').val();
-		alert('Нове ім\'я студента: ' + Student.name);
-	}
-});
+	$('#button_name').on('click', function ChangeName() {
+		if( $('#input_name').val() == '')
+		{
+			alert("Нічого не введено");
+		}
+		else
+		{
+			Student.name = $('#input_name').val();
+			alert('Нове ім\'я студента: ' + Student.name);
+		}
+	});
 
-$('#button_sur').on('click', function ChangeSurname() {
-	if( $('#input_sur').val() == '')
-	{
-		alert("Нічого не введено");
-	}
-	else
-	{
-		Student.surname = $('#input_sur').val();
-		alert('Нове прізвище студента: ' + Student.surname);
-	}
-});
+	$('#button_sur').on('click', function ChangeSurname() {
+		if( $('#input_sur').val() == '')
+		{
+			alert("Нічого не введено");
+		}
+		else
+		{
+			Student.surname = $('#input_sur').val();
+			alert('Нове прізвище студента: ' + Student.surname);
+		}
+	});
 
-$('#button_course').on('click', function MoveToSecondCourse() {
-	Student.course = Student.course + 1;
-	if(Student.course > 4 )
-	{
-		Student.course = 1
-	}
-	alert('Студента переведено на ' + Student.course + ' курс');
-});
+	$('#button_course').on('click', function MoveToSecondCourse() {
+		Student.course = Student.course + 1;
+		if(Student.course > 4 )
+		{
+			Student.course = 1
+		}
+		alert('Студента переведено на ' + Student.course + ' курс');
+	});
 
-$('#info').on('click', function ShowInfo() {
-	alert('Студент: ' + Student.name + ' ' + Student.surname + '\n' + 'Вік: ' + Student.age + '\n' + 'Курс: ' + Student.course);
-});
+	$('#info').on('click', function ShowInfo() {
+		alert('Студент: ' + Student.name + ' ' + Student.surname + '\n' + 'Вік: ' + Student.age + '\n' + 'Курс: ' + Student.course);
+	});
 
-// 5) Реалізувати користувацьку JS-функцію відповідно до варіанту:
-// 		6. Функція, що визначає найменше значення серед заданих чисел.
+	// 5) Реалізувати користувацьку JS-функцію відповідно до варіанту:
+	// 		6. Функція, що визначає найменше значення серед заданих чисел.
 
-$('#min_button').on('click', function Min() {
-	let min;
-	let a = parseInt($('#input_1').val());
-	let b = parseInt($('#input_2').val());
-	let c = parseInt($('#input_3').val());
+	$('#min_button').on('click', function Min() {
+		let min;
+		let a = parseInt($('#input_1').val());
+		let b = parseInt($('#input_2').val());
+		let c = parseInt($('#input_3').val());
 
-	if(a < b && a < c)
-	{
-		min = a;
-	}
-	else if(b < a && b < c)
-	{
-		min = b;
-	}
-	else if(c < a && c < b)
-	{
-		min = c;
-	}
-	alert('Мінімальне значення із введених: ' + min);
+		if(a < b && a < c)
+		{
+			min = a;
+		}
+		else if(b < a && b < c)
+		{
+			min = b;
+		}
+		else if(c < a && c < b)
+		{
+			min = c;
+		}
+		alert('Мінімальне значення із введених: ' + min);
+	});
 });
